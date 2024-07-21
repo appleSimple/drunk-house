a9bd51a04fbbfe42183699e5f05d780f773fdf95
appleSimple <jy_4148@naver.com>
2024-07-21 17:55:46 +0900

    feat: 회원가입 레이아웃 구성

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,48 @@
+'use client';
+import { FormEvent, useRef } from 'react';
+import styles from './page.module.css';
+import { BaseInput } from '@/components/base-input';
+import { BaseButton } from '@/components/base-button';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
+/**
+ * TODO
+ * 1. 아이디 정규식: 공백 미허용, 영어와 숫자만 허용
+ * 2. 비밀번호 정규식: 공백 미허용, 영어, 숫자, 특수문자만 허용. 캡스락 경고창 표시, 자동으로 소문자로 변환
+ * 3. API 연동
+ */
+export default function Signup() {
+  const userNameRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const birthRef = useRef<HTMLInputElement>(null);
+  const nicknameRef = useRef<HTMLInputElement>(null);
+
+  function submit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (userNameRef.current && passwordRef.current) {
+      const loginData = {
+        userName: userNameRef.current.value,
+        password: passwordRef.current.value,
+        // birth: birthRef.current.value,
+        // nickname: nicknameRef.current.value
+      }
+
+      console.log('loginData', loginData);
+    }
+  }
+
+  return (
+    <>
+      <div className={styles.login}>
+        <h1>Signup</h1>
+        <form className={styles.form} onSubmit={submit}>
+          <BaseInput ref={userNameRef} label="아이디" variant="standard" size="small" name="username" autoComplete="username" required />
+          <BaseInput ref={passwordRef} label="비밀번호" variant="standard" size="small" name="password" autoComplete="current-password" required />
+          <DatePicker ref={birthRef} label="생년월일" />
+          <BaseInput ref={nicknameRef} label="닉네임" variant="standard" size="small" name="nickname" autoComplete="nickname" required />
+          <BaseButton type="submit">회원가입</BaseButton>
+        </form>
+      </div>
+    </>
+  );
+}
