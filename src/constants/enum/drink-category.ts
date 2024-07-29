ce3fc67d8959f1c1e29a20af10a6b106f94a8975
appleSimple <jy_4148@naver.com>
2024-07-29 23:03:16 +0900

    feat: drink 카테고리 이넘 생성

---
+++ src/constants/enum/drink-category.ts
@@ -0,0 +1,15 @@
+enum DrinkCategoryEnum {
+  SHOCHU = 'SHOCHU',
+  BEER = 'BEER',
+  MAKGEOLLI = 'MAKGEOLLI',
+  SAKE = 'SAKE',
+  WINE = 'WINE',
+  WHISKY = 'WHISKY',
+  BRANDY = 'BRANDY',
+  RUM = 'RUM',
+  VODKA = 'VODKA'
+}
+
+export {
+  DrinkCategoryEnum,
+}
