2f3362f3fd7090f2b537a2753a7654b8cb95c098
appleSimple <jy_4148@naver.com>
2024-08-03 16:06:48 +0900

    feat: 사용되는 API 추가

---
+++ src/config/api/drink-service.ts
@@ -0,0 +1,29 @@
+import { AxiosResponse } from 'axios';
+import { post, get } from './index';
+import { DrinkCategory } from '@/types/drink';
+
+const DRINK = '/drink';
+
+export type Condition = {
+  category?: DrinkCategory;
+  keyword?: string;
+};
+
+export type Pageable = {
+  page: number | string; // 0부터 시작
+  size: number | string;
+};
+
+const getTotalDrinkList = async (pageable: Pageable, condition?: Condition) => {
+  const obj: Partial<Condition> & Pageable = {
+    ...(condition || {}),
+    page: String(pageable.page),
+    size: String(pageable.size),
+  };
+
+  const params = new URLSearchParams(obj);
+  
+  return get(`${DRINK}?${params.toString()}`);
+};
+
+export { getTotalDrinkList };
