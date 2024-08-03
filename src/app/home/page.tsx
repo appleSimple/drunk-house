439ea6b675386dda0a0298320666461f9342b23b
appleSimple <jy_4148@naver.com>
2024-08-03 16:39:26 +0900

    feat: 버튼 클릭시 필터링

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,22 +4,23 @@ import { useEffect, useState } from 'react';
 import CardList from '@/components/card-list';
 import { Condition, getTotalDrinkList } from '@/config/api/drink-service';
 import { Drink } from '@/types/drink';
+import DrinkCategoryName from '@/constants/enumToName/drink-category';
+import { DrinkCategoryEnum } from '@/constants/enum/drink-category';
 
 export default function Home() {
   const [page, setPage] = useState(0);
   const [size, setSize] = useState(10);
+  const [category, setCategory] = useState<Partial<Condition>>('');
   const [drinkList, setDrinkList] = useState<Drink[]>([]);
 
   useEffect(() => {
     fetchDrinkList();
-  }, []);
+  }, [category]);
   
   async function fetchDrinkList() {
     const condition: Partial<Condition> = {};
-    // if (category) condition.category = category;
+    if (category) condition.category = category;
     // if (keyword) condition.keyword = keyword;
-
-    condition.category = 'SHOCHU';
 
     const pageable = { page, size };
 
@@ -28,11 +29,21 @@ export default function Home() {
     setDrinkList((prev) => [...prev, ...response.data.drinks]);
   }
 
+  function handleCategory(category: string) {
+    console.log('category', category);
+    
+    setCategory(category);
+    setDrinkList([]);
+    setPage(0);
+  }
+
   return (
     <>
       <div className={styles.dashboard}>
-        대시보드
-        <CardList page={page} size={size} drinkList={drinkList} />
+        {Object.keys(DrinkCategoryName).map((category) => (
+          <button key={category} onClick={() => handleCategory(category)}>{DrinkCategoryName[category]}</button>
+        ))}
+        <CardList list={drinkList} />
       </div>
     </>
   );
