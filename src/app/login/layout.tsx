c219c5a8c265275c5f0d2eb1cd9d87bc8b1b5278
appleSimple <jy_4148@naver.com>
2024-08-04 20:05:26 +0900

    style: 로그인 화면 스타일 재정비

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -1,10 +1,19 @@
+'use client';
+import { Typography } from "@mui/material"
+import { useRouter } from "next/navigation";
+import styles from './layout.module.css'
+
 export default function LoginLayout({
   children
 }: {
   children: React.ReactNode
 }) {
+  const router = useRouter();
+
   return (
-    <section>
+    <section className={styles.layout}>
+      <Typography variant="h3" fontWeight={500} align="center" onClick={() => router.push('/login')} className={styles.logo}>🍷 Drunk House</Typography>
+      <Typography variant="h5">로그인을 해주세요</Typography>
       {children}
     </section>
   )
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,14 @@
 'use client';
-import { FormEvent, useRef } from 'react';
+import { FormEvent, useEffect, useRef } from 'react';
 import { useRouter } from 'next/navigation'
 import Link from "next/link";
 import styles from './page.module.css';
 import { BaseInput } from '@/components/base-input';
 import { BaseButton } from '@/components/base-button';
 import { login } from '@/config/api/user-service';
+import { useUserStore } from '@/store/useUserStore';
+import Button from '@mui/material/Button';
+import { TextField } from '@mui/material';
 
 /**
  * TODO
@@ -13,10 +16,16 @@ import { login } from '@/config/api/user-service';
  * 2. 비밀번호 정규식: 공백 미허용, 영어, 숫자, 특수문자만 허용. 캡스락 경고창 표시, 자동으로 소문자로 변환
  */
 export default function Login() {
-  const router = useRouter()
+  const router = useRouter();
   const userNameRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
- 
+  const updateUserInfo = useUserStore((state) => state.updateUserInfo);
+  const reset = useUserStore((state) => state.reset);
+
+  useEffect(() => {
+    reset();
+  }, []);
+  
   async function submit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
     if (userNameRef.current && passwordRef.current) {
@@ -27,6 +36,7 @@ export default function Login() {
 
       try {
         const response = await login(body);
+        updateUserInfo(response.data);
         router.push('/home');
       } catch(error) {
         console.error(error);
@@ -37,13 +47,12 @@ export default function Login() {
   return (
     <>
       <div className={styles.login}>
-        <h1>Login</h1>
         <form className={styles.form} onSubmit={submit}>
-          <BaseInput ref={userNameRef} label="아이디" variant="standard" size="small" name="username" autoComplete="username" required />
-          <BaseInput ref={passwordRef} type='password' label="비밀번호" variant="standard" size="small" name="password" autoComplete="current-password" required />
-          <BaseButton type="submit">로그인</BaseButton>
+          <TextField inputRef={userNameRef} label="아이디" variant="filled" size="small" name="username" autoComplete="username" required />
+          <TextField inputRef={passwordRef} type='password' label="비밀번호" variant="filled" size="small" name="password" autoComplete="current-password" required />
+          <Button type="submit" variant='contained' size='large'>로그인</Button>
         </form>
-        <Link href="/signup">회원가입</Link>
+        <Link href="/signup">회원이 아니신가요?</Link>
       </div>
     </>
   );
