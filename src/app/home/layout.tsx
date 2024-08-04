1f846547dfbfc876274e219db31e93b904cc3bdf
appleSimple <jy_4148@naver.com>
2024-08-04 19:09:01 +0900

    style: 대시보드 스타일 재정비

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,11 +1,17 @@
+'use client';
+import { useRouter } from 'next/navigation';
+import styles from './layout.module.css';
+
 export default function HomeLayout({
   children
 }: {
   children: React.ReactNode
 }) {
+  const router = useRouter();
+
   return (
     <section>
-      <h1>🍷 Drunk House</h1>
+      <h1 onClick={() => router.push('/home')} className={styles.logo}>🍷 Drunk House</h1>
       {children}
     </section>
   )
