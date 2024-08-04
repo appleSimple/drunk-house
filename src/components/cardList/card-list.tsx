92b37dfcf608e78666c6a2531845c8201a74d7da
appleSimple <jy_4148@naver.com>
2024-08-04 17:39:48 +0900

    style: 대시보드 카드 레이아웃 스타일

--- src/components/cardList/card-list.tsx
+++ src/components/cardList/card-list.tsx
@@ -2,21 +2,22 @@
 import React, { useEffect, useState } from 'react';
 import { getTotalDrinkList, Condition } from '@/config/api/drink-service';
 import { Drink, DrinkCategory } from '@/types/drink';
-import Card from './card';
+import styles from './card-list.module.css';
+import Card from '../card';
 
 type Props<T> = {
   list: T[];
 }
 
 function CardList<T,>(props: Props<T>) {
-  const { list } = props;
+  const { list, ...ohterProps } = props;
 
   return (
-    <>
+    <div className={styles.cardList}>
       {list.map((el) => (
-        <Card key={el.id} id={el.id} name={el.name} category={el.category} profile={el.profile} abv={el.abv}  />
+        <Card key={el.id} id={el.id} name={el.name} category={el.category} profile={el.profile} abv={el.abv} />
       ))}
-    </>
+    </div>
   )
 }
 
