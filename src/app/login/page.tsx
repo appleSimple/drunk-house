04d03ac942b1f67cfbed60acbb3519266700ef82
appleSimple <jy_4148@naver.com>
2024-08-04 17:40:02 +0900

    feat: 로그인 화면에 회원가입 링크 추가

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,8 @@
 'use client';
-import { useRouter } from 'next/navigation'
-import styles from './page.module.css';
 import { FormEvent, useRef } from 'react';
+import { useRouter } from 'next/navigation'
+import Link from "next/link";
+import styles from './page.module.css';
 import { BaseInput } from '@/components/base-input';
 import { BaseButton } from '@/components/base-button';
 import { login } from '@/config/api/user-service';
@@ -39,9 +40,10 @@ export default function Login() {
         <h1>Login</h1>
         <form className={styles.form} onSubmit={submit}>
           <BaseInput ref={userNameRef} label="아이디" variant="standard" size="small" name="username" autoComplete="username" required />
-          <BaseInput ref={passwordRef} label="비밀번호" variant="standard" size="small" name="password" autoComplete="current-password" required />
+          <BaseInput ref={passwordRef} type='password' label="비밀번호" variant="standard" size="small" name="password" autoComplete="current-password" required />
           <BaseButton type="submit">로그인</BaseButton>
         </form>
+        <Link href="/signup">회원가입</Link>
       </div>
     </>
   );
