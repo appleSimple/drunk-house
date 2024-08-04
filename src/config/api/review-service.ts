da9514b8dc84b45b041168302342b52182ca4a02
appleSimple <jy_4148@naver.com>
2024-08-04 20:06:48 +0900

    chore: 리뷰 api 서비스 추가

---
+++ src/config/api/review-service.ts
@@ -0,0 +1,31 @@
+import { AxiosResponse } from 'axios';
+import { post, get } from './index';
+import { DrinkCategory } from '@/types/drink';
+
+const DRINK = '/review';
+
+export type Condition = {
+  category?: DrinkCategory;
+  keyword?: string;
+  memberId: string;
+};
+
+export type Pageable = {
+  page: number | string; // 0부터 시작
+  size: number | string;
+};
+
+const getReviewList = async (userId: number, pageable: Pageable, condition?: Condition) => {
+  const obj: Partial<Condition> & Pageable = {
+    ...(condition || {}),
+    page: String(pageable.page),
+    size: String(pageable.size),
+    memberId: String(userId),
+  };
+
+  const params = new URLSearchParams(obj);
+  
+  return get(`${DRINK}?${params.toString()}`);
+};
+
+export { getReviewList };
