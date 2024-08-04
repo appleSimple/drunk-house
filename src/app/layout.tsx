ba7c95368fb720565ed699c028d7941f052274ea
appleSimple <jy_4148@naver.com>
2024-08-04 20:04:34 +0900

    style: 폰트 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
 'use client';
-import { Hahmlet } from "next/font/google";
+import { Roboto } from "next/font/google";
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import Container from "@mui/material/Container/Container";
 import "./globals.css";
 import GlobalStyles from "@mui/material/GlobalStyles";
 
-const hahmlet = Hahmlet({
+const roboto = Roboto({
+  weight: '400',
   subsets: ['latin'],
-  variable: '--font-hahmlet',
   display: 'swap',
 })
 
@@ -22,7 +22,7 @@ export default function RootLayout({
       <meta charSet="utf-8" />
       <title>My page</title>
       <meta name="description" content="My page description" />
-      <body className={`${hahmlet.className} ${hahmlet.variable}`}>
+      <body className={`${roboto.className} ${roboto.variable}`}>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <Container maxWidth="sm" sx={{ backgroundColor: 'white', minWidth: '360px' }} className="container">
           {children}
